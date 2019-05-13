91fa62f3cd0140c96291929675401c7d67230db9
Viacheslav Kirichenko <botinkakvd@gmail.com>
2019-05-13 12:11:19 +0300

    init upload
    init upload

---
+++ SQLighter.py
@@ -0,0 +1,42 @@
+# -*- coding:  utf-8 -*-
+import sqlite3
+
+class SQLighter:
+
+    def __init__(self, database):
+        self.connection = sqlite3.connect(database)
+        self.cursor = self.connection.cursor()
+
+    def AddUserFirstTime(self,id,username):
+        with self.connection:
+            command = 'INSERT INTO "USERS" (`id`,`username`) VALUES ('+str(id)+',"'+str(username)+'");'
+            result = self.cursor.execute(command).fetchall()
+            return len(result)
+
+    def UpdateUserInfo(self,id,key,value):
+        with self.connection:
+            command = 'UPDATE "USERS" SET "'+str(key)+'" = "'+str(value)+'" WHERE "id" = "'+str(id)+'"'
+            result = self.cursor.execute(command).fetchall()
+            return len(result)
+
+    def UserExists(self,id,username):
+        with self.connection:
+           command = 'SELECT id FROM USERS WHERE id = "' +str(id)+'"'
+           idresult = self.cursor.execute(command).fetchall()
+           command = 'SELECT username FROM USERS WHERE username = "' +str(username)+'"'
+           usernameresult = self.cursor.execute(command).fetchall()
+        if len(idresult) == 0:
+           self.AddUserFirstTime(id,username)
+        return len(usernameresult)
+    
+    def WhoAmI(self,id):
+        with self.connection:
+            return self.cursor.execute('SELECT * FROM "USERS" WHERE id = ?', (id,)).fetchall()[0]
+    
+    def Whois(self,username):
+        with self.connection:
+            return self.cursor.execute('SELECT * FROM "USERS" WHERE username = ?', (username,)).fetchall()[0]
+            
+    def close(self):
+        #""" ��������� ������� ���������� � �� """
+        self.connection.close()
---
+++ lebot.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+from SQLighter import SQLighter
+import random
+import threading
+import config
+import telebot
+import time
+from datetime import datetime,timedelta
+
+rules = ''' Hello!
+Both languages are allowed.
+Feel free to ask, if you do not understand something in a conversation.
+Don't be rude
+
+Only English chat (English practice for Russians): 
+@elish_practice
+Native speakers are welcomed here. Please help us, like we help you. Thank you.
+
+Оба языка разрешены.
+Спрашивайте, если что-то непонятно.
+Не будьте грубыми.
+Старайтесь уместить мысль в одно сообщение.
+
+Специально для русских: материться можно, но только если этого требует контекст разговора.
+#RULES
+
+================
+Bot added to chat. 
+write @lengrubot and press start to fill your profile.
+/me - for info about you
+/info [nickname] - about anyone else.
+#bot_info
+================
+'''
+
+def langlevel(level):
+   result = '['+('#'*level) + ('-' *(5-level))+']'
+   return result
+
+bot = telebot.TeleBot(config.token)	
+
+# command
+# /info [username]  -- info about one username
+# /list             -- list all username with info (потом, в этйо команде особо нет необходимости)
+# /me               -- list info about me
+# запрос инфы для нового пользователя и добавление в БД
+# вывод инфы
+
+markuplang = telebot.types.InlineKeyboardMarkup()
+markuplevel = telebot.types.InlineKeyboardMarkup()
+markupgmt = telebot.types.InlineKeyboardMarkup()
+#create buttons for eng ru
+markuplang.row(
+    telebot.types.InlineKeyboardButton("English", callback_data="lang=English"),
+    telebot.types.InlineKeyboardButton("Russian", callback_data="lang=Russian")
+    )
+# create buttons for lang level    
+markuplevel.row(*[telebot.types.InlineKeyboardButton(str(i), callback_data="level="+str(i)) for i in range(0,6,1)])
+#create buttons for UTC
+for j in range(-14,12,8):
+    markupgmt.row(*[telebot.types.InlineKeyboardButton(str(i), callback_data="utc="+str(i)) for i in range(j,13,1)])
+
+@bot.message_handler(commands=['start'])
+def test_message(message):
+    bot.send_message(message.from_user.id, "Choose your native (main) language", reply_markup = markuplang)
+
+#@bot.message_handler(commands=['rules'])
+#def send_message(message):      
+#        bot.send_message(message.chat.id,rules)
+
+
+@bot.message_handler(commands=['me'])
+def send_messages(message):	
+    db_worker = SQLighter(config.database_name)
+    db_worker.UserExists(message.from_user.id, message.from_user.username)
+    user_id, user_username, user_lang, user_level, user_UTC = db_worker.WhoAmI(message.from_user.id)
+    db_worker.close()
+    if user_UTC is None: 
+        user_time = "Unknown"
+    else:
+        user_time = (datetime.utcnow() + timedelta(hours=user_UTC)).strftime("%H:%M")
+    if user_level is None:
+        user_level = "Unknown"
+    else:
+        user_level = langlevel(user_level)
+    if user_username == "None":
+        user_username = str(message.from_user.first_name) + " "+ str(message.from_user.last_name)
+    else:
+        user_username = "@"+user_username
+    response = str(user_username)+' is '+str(user_lang)+' native speaker. Other language level is '+user_level+'. Current time: '+user_time
+    bot.send_message(message.chat.id, response) 
+
+@bot.message_handler(commands=['info'])
+def send_messages(message):
+    username = message.text[6:len(message.text)]
+    if username.startswith("@"): username = username[1:len(message.text)] 
+    db_worker = SQLighter(config.database_name)
+    if db_worker.UserExists(message.from_user.id, username) != 0:
+        user_id, user_username, user_lang, user_level, user_UTC = db_worker.Whois(username)
+        #user_time = datetime.utcnow() + timedelta(hours=user_UTC)
+        if user_UTC is None: 
+            user_time = "Unknown"
+        else:
+            user_time = (datetime.utcnow() + timedelta(hours=user_UTC)).strftime("%H:%M")
+        if user_level is None:
+            user_level = "Unknown"
+        else:
+            user_level = langlevel(user_level)
+        response = '@'+str(user_username)+' is '+str(user_lang)+' native speaker. Other language level is '+user_level+'. Current time: '+user_time.strftime("%H:%M")
+    else:
+        response = 'User '+username+ " does not exists."        
+    db_worker.close()
+    
+    bot.send_message(message.chat.id, response)
+
+@bot.callback_query_handler(func=lambda call:True)
+def callback_answer(callback_query: telebot.types.CallbackQuery): #И отвечаем на него
+    bot.answer_callback_query(
+            callback_query.id,
+            text='Choosed '+callback_query.data,
+            show_alert=False
+            )
+    if (callback_query.data).startswith("lang=") == True:
+        bot.send_message(
+            chat_id=callback_query.message.chat.id, 
+            text="Type your knowledge level of other language", 
+            reply_markup = markuplevel
+            )
+        #adduserlanguage if userexists
+        db_worker = SQLighter(config.database_name)
+        db_worker.UserExists(callback_query.from_user.id,callback_query.from_user.username)
+        db_worker.UpdateUserInfo(callback_query.from_user.id,'lang',callback_query.data[5:])
+        db_worker.close()
+    if (callback_query.data).startswith("level=") == True:
+        bot.send_message(
+            chat_id=callback_query.message.chat.id, 
+            text="Choose your timezone (+3 for Moscow, for example)",
+            reply_markup = markupgmt
+            )  
+        #adduserlevel
+        db_worker = SQLighter(config.database_name)
+        db_worker.UpdateUserInfo(callback_query.from_user.id,'level',callback_query.data[6:])
+        db_worker.close()
+    if (callback_query.data).startswith("utc=") == True:
+        bot.send_message(
+            chat_id=callback_query.message.chat.id, 
+            text="Thank you! You can try '/me' command."
+            )  
+        #adduserUTC
+        db_worker = SQLighter(config.database_name)
+        db_worker.UpdateUserInfo(callback_query.from_user.id,'UTC',callback_query.data[4:])
+        db_worker.close()
+    
+
+if __name__ == '__main__':
+    random.seed()
+    bot.polling(none_stop=True)
